23c89fee86363e5bf493ba7d3afb24a367790ebc
Ankush Singh <83535682+ankushsingh24@users.noreply.github.com>
2023-02-09 08:29:34 +0530

    Time: 638 ms (82.11%), Space: 83.4 MB (43.90%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) 
+    {
+        unordered_set<string> count[26];
+        for(auto &s: ideas)
+            count[s[0] - 'a'].insert(s.substr(1));
+        
+        long long res = 0;
+        for(int i=0 ; i<26 ; i++)
+        {
+            for(int j=i+1 ; j<26 ; j++)
+            {
+                long long c1 = 0L, c2 = 0L;
+                
+                for(auto &c: count[i])
+                    if(count[j].find(c) == count[j].end())  
+                        c1++;
+                
+                for(auto &c: count[j])
+                    if(count[i].find(c) == count[i].end())
+                        c2++;
+                
+                res += c1 * c2;
+            }
+        }
+        
+        return res * 2;
+    }
+};
