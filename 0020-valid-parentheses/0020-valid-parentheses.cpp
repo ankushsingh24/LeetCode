10853fa0efa61614c9049fe338ca56072a92e0bb
Ankush Singh <83535682+ankushsingh24@users.noreply.github.com>
2023-04-10 15:09:43 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (56.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char> st;
+        
+        for(int i=0 ; i<s.size() ; i++)
+        {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+                st.push(s[i]);
+            
+            else
+            {
+                // char top = st.top();
+                
+                if(st.empty() || 
+                   (s[i] == ')' && st.top() != '(') || 
+                   (s[i] == '}' && st.top() != '{') || 
+                   (s[i] == ']' && st.top() != '['))
+                    return false;
+                
+                st.pop();
+            }
+        }
+        
+        return st.empty();
+    }
+};
