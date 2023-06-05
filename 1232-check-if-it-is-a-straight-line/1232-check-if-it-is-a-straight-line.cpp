9aaad25d3b3d3315d3ab9d0a849b51ba45671726
Ankush Singh <83535682+ankushsingh24@users.noreply.github.com>
2023-06-05 16:27:21 +0530

    Time: 8 ms (73.14%), Space: 10 MB (63.27%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -6,15 +6,6 @@ public:
         
         for(int i=2 ; i<n ; i++)
         {
-            int x1 = coordinates[i-2][0];
-            int y1 = coordinates[i-2][1];
-            
-            int x2 = coordinates[i-1][0];
-            int y2 = coordinates[i-1][1];
-            
-            int x3 = coordinates[i][0];
-            int y3 = coordinates[i][1];
-            
             int dy1 = coordinates[i-1][1] - coordinates[i-2][1];
             int dx1 = coordinates[i-1][0] - coordinates[i-2][0];
             int dy2 = coordinates[i][1] - coordinates[i-1][1];
