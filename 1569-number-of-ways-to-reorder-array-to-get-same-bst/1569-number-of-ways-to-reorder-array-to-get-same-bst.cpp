a72a5932af5a62f37ab90c86e9b8ae536fea1c73
Ankush Singh <83535682+ankushsingh24@users.noreply.github.com>
2023-06-16 22:12:26 +0530

    Time: 468 ms (23.17%), Space: 163.2 MB (46.36%) - LeetHub

---
+++ 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<long long>> table;
+    long long mod = 1e9 + 7;
+    
+    long long dfs(vector<int> &nums)
+    {
+        int n = nums.size();
+        if(n < 3)
+            return 1;
+        
+        vector<int> left, right;
+        
+        for(int i=1 ; i<n ; i++)
+        {
+            if(nums[i] < nums[0])
+                left.push_back(nums[i]);
+            else
+                right.push_back(nums[i]);
+        }
+        
+        long long leftWays = dfs(left) % mod;
+        long long rightWays = dfs(right) % mod;
+        
+        return (((leftWays * rightWays) % mod) * table[n-1][left.size()]) % mod;
+    }
+    
+    int numOfWays(vector<int>& nums) 
+    {
+        int n = nums.size();
+        
+        table.resize(n+1);
+        for(int i=0 ; i<n+1 ; i++)
+        {
+            table[i] = vector<long long> (i+1, 1);
+            for(int j=1 ; j<i ; j++)
+                table[i][j] = (table[i-1][j-1] + table[i-1][j]) % mod;
+        }
+        
+        return (dfs(nums) - 1) % mod;
+    }
+};
