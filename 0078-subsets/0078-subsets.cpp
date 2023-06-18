0c1e413c8deb56ad744efffdff5f153994ac797f
Ankush Singh <83535682+ankushsingh24@users.noreply.github.com>
2023-06-18 17:37:42 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (61.47%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void subsets(vector<vector<int>> &res, vector<int> &nums, vector<int> &vec, int begin)
+    {
+        res.push_back(vec);
+        for(int i=begin ; i != nums.size() ; i++)
+        {
+            vec.push_back(nums[i]);
+            subsets(res, nums, vec, i+1);
+            vec.pop_back();
+        }
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        vector<int> vec;
+        
+        subsets(res, nums, vec, 0);
+        return res;
+    }
+};
