2069555f3e530dc9cd0140c2cfb3f0bfd54f3962
Ankush Singh <83535682+ankushsingh24@users.noreply.github.com>
2023-07-03 13:23:01 +0530

    Time: 3 ms (78.34%), Space: 7 MB (19.51%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) 
+    {
+        if(s.size() != goal.size())
+            return false;
+        
+        if(s == goal && set<char> (s.begin(), s.end()).size() < s.size())
+            return true;
+        
+        vector<int> v;
+        for(int i=0 ; i<s.size() ; i++)
+            if(s[i] != goal[i])
+                v.push_back(i);
+        
+        return v.size() == 2 && s[v[0]] == goal[v[1]] && s[v[1]] == goal[v[0]];
+    }
+};
