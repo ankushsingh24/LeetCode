84ca3d90731e8dc7b6946b4530ab86ab8753bb80
Ankush Singh <83535682+ankushsingh24@users.noreply.github.com>
2023-07-24 13:32:15 +0530

    Time: 4 ms (40.18%), Space: 5.9 MB (62.65%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
+    double rec(double x, long long n)
+    {
+        if(n == 0)
+            return 1;
+        
+        if(n < 0)
+            return 1.0 / rec(x, -1*n);
+        
+        if(n%2 == 1)
+            return x * rec(x*x, (n-1)/2);
+        
+        else
+            return rec(x*x, n/2);
+    }
+    
     double myPow(double x, int n) 
     {
-        return pow(x, n);    
+        return rec(x, n);    
     }
 };
