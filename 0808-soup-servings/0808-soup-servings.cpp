792e2e062126537f1869c876c06ccfe12da3b13a
Ankush Singh <83535682+ankushsingh24@users.noreply.github.com>
2023-07-29 13:19:39 +0530

    Time: 2 ms (87.45%), Space: 6.4 MB (87.45%) - LeetHub

---
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    double dp[200][200];
+    
+    double f(int a, int b)
+    {
+        if(a <= 0 && b <= 0)
+            return 0.5;
+        
+        if(a <= 0)
+            return 1;
+        
+        if(b <= 0)
+            return 0;
+        
+        if(dp[a][b] > 0)
+            return dp[a][b];
+        
+        dp[a][b] = 0.25 * (f(a-4, b) + f(a-3, b-1) + f(a-2, b-2) + f(a-1, b-3));
+        
+        return dp[a][b];
+    }
+    
+    double soupServings(int n) 
+    {
+        return n > 4800 ? 1.0 : f((n + 24) / 25, (n + 24) / 25);    
+    }
+};
